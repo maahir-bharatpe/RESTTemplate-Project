c24721dfe69f895cc8db7e56ac2e8136ff74e5b5
maahir-bharatpe <91525023+maahir-bharatpe@users.noreply.github.com>
2021-09-29 16:20:43 +0530

    Update needed
    Understand the working of constructors in nested classes.

--- src/main/java/com/resttemp/demo/entities/MyArticles.java
+++ src/main/java/com/resttemp/demo/entities/MyArticles.java
@@ -17,7 +17,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
-
+//
 @Entity
 @Table
 public class MyArticles {
